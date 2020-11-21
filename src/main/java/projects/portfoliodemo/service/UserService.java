c63ecc17a3714acb0fe4f004bc808fd142d7a339
Jakub-Gembiak <jakub.gembiak@gmail.com>
2020-11-21 21:33:19 +0100

    register-user :: user registration completed

--- src/main/java/projects/portfoliodemo/service/UserService.java
+++ src/main/java/projects/portfoliodemo/service/UserService.java
@@ -1,12 +1,43 @@
 package projects.portfoliodemo.service;
 
+import antlr.BaseAST;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import projects.portfoliodemo.converter.UserConverter;
+import projects.portfoliodemo.domain.model.User;
+import projects.portfoliodemo.domain.repositories.UserRepository;
+import projects.portfoliodemo.exception.UserAlreadyExistsException;
 import projects.portfoliodemo.web.command.RegisterUserCommand;
 
-@Service
+import javax.transaction.Transactional;
+import java.util.Set;
+
+@Service @Transactional
+@Slf4j @RequiredArgsConstructor
 public class UserService {
 
+    private final UserConverter userConverter;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
     public Long create(RegisterUserCommand registerUserCommand) {
-        return null;
+        log.debug("Dane użytkownika do zapisania: {}", registerUserCommand);
+
+        User userToCreate = userConverter.from(registerUserCommand);
+        log.debug("Uzyskany obiekt użytkownika do zapisu: {}", userToCreate);
+        if (userRepository.existsByUsername(userToCreate.getUsername())) {
+            log.debug("Próba rejestracji na istniejącego użytkownika");
+            throw new UserAlreadyExistsException(String.format("Użytkownik %s już istnieje", userToCreate.getUsername()));
+        }
+
+        userToCreate.setActive(Boolean.TRUE);
+        userToCreate.setRoles(Set.of("ROLE_USER"));
+        userToCreate.setPassword(passwordEncoder.encode(userToCreate.getPassword()));
+        userRepository.save(userToCreate);
+        log.debug("Zapisany użytkownik: {}", userToCreate);
+
+        return userToCreate.getId();
     }
 }
