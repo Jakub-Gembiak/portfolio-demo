bdd1ce34b01145ffc6fcecf62cd51b41644db526
Jakub-Gembiak <jakub.gembiak@gmail.com>
2020-11-22 14:13:34 +0100

    project-add :: uzupełnienie CreateProjectCommand, dodanie formularza

--- src/main/java/projects/portfoliodemo/web/controller/AddNewProjectController.java
+++ src/main/java/projects/portfoliodemo/web/controller/AddNewProjectController.java
@@ -1,5 +1,7 @@
 package projects.portfoliodemo.web.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +10,11 @@ import projects.portfoliodemo.web.command.CreateProjectCommand;
 
 @Controller
 @RequestMapping("/project")
+@Slf4j @RequiredArgsConstructor
 public class AddNewProjectController {
 
-    @GetMapping
-    public String getProjectAdd(Model model) {
+    @GetMapping("/add")
+    public String getAddProjectPage(Model model) {
         model.addAttribute("createProjectCommand", new CreateProjectCommand());
         return "project/add";
     }
