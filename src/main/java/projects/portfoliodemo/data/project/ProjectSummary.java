d51278d579be884c3fbd917863705a422e3e262d
Jakub-Gembiak <jakub.gembiak@gmail.com>
2020-11-22 15:36:51 +0100

    project-list :: struktura klas

---
+++ src/main/java/projects/portfoliodemo/data/project/ProjectSummary.java
@@ -0,0 +1,16 @@
+package projects.portfoliodemo.data.project;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor @Builder
+public class ProjectSummary {
+
+    private Long id;
+    private String name;
+    private String username;
+    private String url;
+    private String description;
+}
