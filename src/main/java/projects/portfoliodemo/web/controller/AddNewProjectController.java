3b69122726b57e91b81b8e9eabef13ac3fbb5a2f
Jakub-Gembiak <jakub.gembiak@gmail.com>
2020-11-22 13:47:34 +0100

    project-add :: metoda get w AddNewProjectController

--- src/main/java/projects/portfoliodemo/web/controller/AddNewProjectController.java
+++ src/main/java/projects/portfoliodemo/web/controller/AddNewProjectController.java
@@ -1,4 +1,18 @@
 package projects.portfoliodemo.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import projects.portfoliodemo.web.command.CreateProjectCommand;
+
+@Controller
+@RequestMapping("/project")
 public class AddNewProjectController {
+
+    @GetMapping
+    public String getProjectAdd(Model model) {
+        model.addAttribute("createProjectCommand", new CreateProjectCommand());
+        return "project/add";
+    }
 }
